b405afe236aef6c8b9e3f395c07cc10ac42a365f
Ajay Kumar <ajay.kumar@jgu.edu.in>
2025-07-18 08:09:04 +0530

    minor fix

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/products');
 });
 
 
